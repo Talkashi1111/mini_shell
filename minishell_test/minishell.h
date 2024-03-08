de81525aa2cd4b2f48790f3118baa07a86dd3e3d
Tal Kashi <143966589+Talkashi1111@users.noreply.github.com>
2024-03-08 20:10:08 +0100

    minishell_v1 (#6)
    Co-authored-by: aiko <aiko@imac-de-lab7.home>

---
+++ minishell_test/minishell.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: achappui <achappui@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/23 17:37:04 by achappui          #+#    #+#             */
+/*   Updated: 2024/02/23 17:38:35 by achappui         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+#include <stdio.h>
+#include <readline/readline.h>
+#include <readline/history.h>
+#include <stdlib.h>
+#include "libft.h"
+
+
+#endif
