906138a2b298b6b36aea67115498f4d68512b25b
achappui <irooyella@gmail.com>
2024-04-11 00:37:58 +0200

    redi start

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,7 +6,7 @@
 /*   By: achappui <achappui@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/23 17:37:04 by achappui          #+#    #+#             */
-/*   Updated: 2024/04/10 14:54:41 by achappui         ###   ########.fr       */
+/*   Updated: 2024/04/10 22:02:19 by achappui         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -153,14 +153,13 @@ void			display_tree(t_node *node);
 void			free_tree(t_node *node);
 
 /* executor */
+int				apply_redirections(t_node *node, t_minishell *info);
 int				ft_run(t_node *node, t_minishell *info);
 void			ft_free_pipes(t_minishell *info);
 int				ft_open_pipes(t_node *node, t_minishell *info);
 int				wildcard_handler(t_token_list **token, t_minishell *info);
 int				remove_quotes(t_token_list *args, t_minishell *info);
-char			expand_dollars(t_token_list *args, t_minishell *info);
-unsigned int	get_varname_len(char *str);
-unsigned int	count_final_length(char *str, t_minishell *info);
+char			expand_dollars(t_token_list **args, t_minishell *info);
 int 			execute_non_builtin(t_node *node, t_minishell *info);
 void    		free_tokens_and_tree(t_minishell *info);
 int 			ft_wait_pid(int child_pid, t_minishell *info);
