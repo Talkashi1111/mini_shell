d82ebfdee1d887fd32f774019808d93ff8984b0e
Tal Kashi <143966589+Talkashi1111@users.noreply.github.com>
2024-04-13 19:10:11 +0200

    builtin norminette and some remarks on dollar implementation (#20)

--- srcs/builtin/cd_builtin.c
+++ srcs/builtin/cd_builtin.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   cd_builtin.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: achappui <achappui@student.42.fr>          +#+  +:+       +#+        */
+/*   By: tkashi <tkashi@student.42lausanne.ch>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/04/02 17:44:37 by achappui          #+#    #+#             */
-/*   Updated: 2024/04/10 13:56:52 by achappui         ###   ########.fr       */
+/*   Created: 2024/04/12 21:49:54 by tkashi            #+#    #+#             */
+/*   Updated: 2024/04/12 22:15:17 by tkashi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,13 +15,51 @@
 #include <string.h>
 #include "minishell.h"
 
-int ft_cd(char *args[], t_minishell *info)
+int	ft_chdir(t_minishell *info, char *new_path, char *curr_path, char *arg)
+{
+	int	err;
+
+	if (chdir(new_path) == -1)
+	{
+		err = errno;
+		ft_fprintf(STDERR_FILENO, "cd: %s: %s\n", new_path, strerror(err));
+		return (err);
+	}
+	if (update_or_add_envp(info, "OLDPWD=", curr_path) == MALLOC_ERROR)
+		return (MALLOC_ERROR);
+	err = ft_getcwd(curr_path, PATH_MAX);
+	if (err != OK)
+		return (err);
+	if (update_or_add_envp(info, "PWD=", curr_path) == MALLOC_ERROR)
+		return (MALLOC_ERROR);
+	if (arg && ft_strncmp(arg, "-", sizeof("-")) == 0)
+		ft_printf("%s\n", curr_path);
+	return (OK);
+}
+
+int	ft_cd2(t_minishell *info, char *path, char **new_path)
+{
+	if (ft_strncmp(path, "-", sizeof("-")) == 0)
+	{
+		*new_path = find_envp_arg(info->envp, "OLDPWD", ft_strlen("OLDPWD"));
+		if (*new_path == NULL || *new_path[0] == '\0')
+		{
+			ft_fprintf(STDERR_FILENO, "cd: OLDPWD not set\n");
+			return (NOT_FOUND);
+		}
+	}
+	else
+		*new_path = path;
+	return (OK);
+}
+
+int	ft_cd(char *args[], t_minishell *info)
 {
 	int		err;
 	char	*new_path;
 	char	curr_path[PATH_MAX];
 
-	err = ft_getcwd(curr_path, sizeof(curr_path));
+	err = ft_getcwd(curr_path, PATH_MAX);
 	if (err != OK)
 		return (err);
 	if (args[1] == NULL || ft_strncmp(args[1], "~", sizeof("~")) == 0)
@@ -35,32 +73,8 @@ int ft_cd(char *args[], t_minishell *info)
 	}
 	else if (args[1] != NULL)
 	{
-		if (ft_strncmp(args[1], "-", sizeof("-")) == 0)
-		{
-			new_path = find_envp_arg(info->envp, "OLDPWD", ft_strlen("OLDPWD"));
-			if (new_path == NULL || new_path[0] == '\0')
-			{
-				ft_fprintf(STDERR_FILENO, "cd: OLDPWD not set\n");
-				return (NOT_FOUND);
-			}
-		}
-		else
-			new_path = args[1];
+		if (ft_cd2(info, args[1], &new_path) == NOT_FOUND)
+			return (NOT_FOUND);
 	}
-	if (chdir(new_path) == -1)
-	{
-		err = errno;
-		ft_fprintf(STDERR_FILENO, "cd: %s: %s\n", new_path, strerror(err));
-		return (err);
-	}
-	if (update_or_add_envp(info, "OLDPWD=", curr_path) == MALLOC_ERROR)
-		return (MALLOC_ERROR);
-	err = ft_getcwd(curr_path, sizeof(curr_path));
-	if (err != OK)
-		return (err);
-	if (update_or_add_envp(info, "PWD=", curr_path) == MALLOC_ERROR)
-		return (MALLOC_ERROR);
-	return (OK);
+	return (ft_chdir(info, new_path, curr_path, args[1]));
 }
-
-
--- srcs/builtin/exit_builtin.c
+++ srcs/builtin/exit_builtin.c
@@ -6,20 +6,19 @@
 /*   By: tkashi <tkashi@student.42lausanne.ch>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/07 16:19:55 by tkashi            #+#    #+#             */
-/*   Updated: 2024/04/07 16:36:24 by tkashi           ###   ########.fr       */
+/*   Updated: 2024/04/13 10:05:12 by tkashi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 #include "minishell.h"
 
-// TODO: check that we didn't forget to free something
 int	ft_exit(char *args[], t_minishell *info)
 {
-    free_args(args);
+	free_args(args);
 	free_tree(info->tree);
 	free_token_list(info->token_list);
-    free_args(info->envp);
+	free_args(info->envp);
 	exit(info->last_exit_status);
 	return (OK);
 }
