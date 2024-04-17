4b91d5734f5e5d7b44b46e32c3acc451dd4087db
Tal Kashi <143966589+Talkashi1111@users.noreply.github.com>
2024-04-17 16:10:35 +0200

    filllheredoc (#28)
    Co-authored-by: Tal Kashi <tkashi@c3r5s2.42lausanne.ch>

--- srcs/executor/fill_heredoc.c
+++ srcs/executor/fill_heredoc.c
@@ -3,20 +3,45 @@
 /*                                                        :::      ::::::::   */
 /*   fill_heredoc.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: achappui <achappui@student.42.fr>          +#+  +:+       +#+        */
+/*   By: tkashi <tkashi@student.42lausanne.ch>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/17 11:51:11 by tkashi            #+#    #+#             */
-/*   Updated: 2024/04/17 13:57:51 by achappui         ###   ########.fr       */
+/*   Updated: 2024/04/17 15:56:44 by tkashi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+void	print_heredoc(char *eof, int saved_streams[2], t_minishell *info, int fd)
+{
+	char	*line;
+	unsigned int eof_len;
+
+	eof_len = ft_strlen(eof);
+	while (TRUE)
+	{
+		line = get_next_line(saved_streams[0]);
+		if (!line || ((ft_strncmp(line, eof, eof_len) == 0) && line[eof_len] == '\n'))
+		{
+			info->last_exit_status = errno;
+			if (line)
+				free(line);
+			break ;
+		}
+		write(saved_streams[1], ">", 1);
+		if (write(fd, line, ft_strlen(line)) < 0)
+		{
+			ft_fprintf(STDERR_FILENO, "write: %s\n", strerror(errno));
+			free(line);
+			return ;
+		}
+		free(line);
+	}
+}
+
 int	fill_heredoc(int saved_streams[2], char *eof, t_minishell *info)
 {
-	char			*line;
-	unsigned int	eof_len;
-	int				fd;
+	int	fd;
 
 	fd = open(".heredoc", O_WRONLY | O_TRUNC | O_APPEND, 0777); //pour les perm autre chose ?
 	if (fd == -1)
@@ -25,54 +50,9 @@ int	fill_heredoc(int saved_streams[2], char *eof, t_minishell *info)
 		ft_fprintf(STDERR_FILENO, "open: %s\n", strerror(errno));
 		return (info->last_exit_status);
 	}
-	eof_len = ft_strlen(eof);
+	ft_fprintf(saved_streams[1], ">");
 	//write(saved_streams[1], ">", 1); //checker le write ? //PROBLEME DE VITESSE d'APPARITION DU >
-	while (TRUE)
-	{
-		write(saved_streams[1], ">", 1);
-		line = get_next_line(saved_streams[0]);
-		if (!line || ((ft_strncmp(line, eof, eof_len) == 0) && line[eof_len] == '\n'))
-		{
-			info->last_exit_status = errno;
-			if (!line)
-			{
-				ft_fprintf(STDERR_FILENO, "read: %s\n", strerror(errno));
-				ft_exit(NULL, info);
-			}
-			free(line);
-			break ;
-		}
-
-		if (write(fd, line, ft_strlen(line)) < 0)
-		{
-			ft_fprintf(STDERR_FILENO, "write: %s\n", strerror(errno));
-			free(line);
-			ft_exit(NULL, info);
-		}
-		free(line);
-	}
-	/* line = get_next_line(saved_streams[0]);
-	if (!line)
-	{
-		info->last_exit_status = errno;
-		ft_fprintf(STDERR_FILENO, "read: %s\n", strerror(errno));
-		ft_exit(NULL, info);
-	}
-	//write(fd, line, ft_strlen(line)); //checker le write
-	while (!(ft_strncmp(line, eof, eof_len) == 0 && line[eof_len] == '\n'))
-	{
-		free(line);
-		write(saved_streams[1], ">", 1); //checker le write ?
-		line = get_next_line(saved_streams[0]);
-		if (!line)
-		{
-			info->last_exit_status = errno;
-			ft_fprintf(STDERR_FILENO, "read: %s\n", strerror(errno));
-			ft_exit(NULL, info);
-		}
-		write(fd, line, ft_strlen(line)); //checker le write
-	}
-	free(line); */
+	print_heredoc(eof, saved_streams, info, fd);
 	if (close(fd) == -1)
 	{
 		info->last_exit_status = errno;
