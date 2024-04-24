b9185f98aaf6530ec62a138f76c406dc48ac064d
Talkashi1111 <taltol20051@gmail.com>
2024-04-24 14:04:47 +0200

    changes for dollars norminette

--- srcs/executor/expand_dollars.c
+++ srcs/executor/expand_dollars.c
@@ -6,44 +6,50 @@
 /*   By: tkashi <tkashi@student.42lausanne.ch>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/02 17:43:53 by achappui          #+#    #+#             */
-/*   Updated: 2024/04/23 10:26:58 by tkashi           ###   ########.fr       */
+/*   Updated: 2024/04/23 23:56:06 by tkashi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-long long	with_dollar(char **str, t_dollar_info *dollar_info, unsigned int *len, t_minishell *info)
+void	search_var_len_dollar(t_minishell *info, char **str,
+	t_dollar_info *dollar_info, long long *i)
 {
-	char			*delimiters;
 	unsigned int	varname_len;
+	char			*delimiters;
+
+	delimiters = " \t\n\v\f\r'*\"$=\0";
+	varname_len = 0;
+	while (ft_strchr(delimiters, (*str + 1)[varname_len]) == 0)
+		varname_len++;
+	(dollar_info->seq) = find_envp_arg(info->envp, *str + 1, varname_len);
+	if ((dollar_info->seq))
+		*i = ft_strlen((dollar_info->seq));
+	*str += varname_len + 1;
+}
+
+long long	with_dollar(char **str, t_dollar_info *dollar_info,
+	unsigned int *len, t_minishell *info)
+{
 	long long		i;
 
 	i = 0;
-	delimiters = " \t\n\v\f\r'*\"$=\0";
 	if ((*str)[i + 1] == '?')
 	{
 		(dollar_info->seq) = ft_itoa(info->last_exit_status);
-		if (!(dollar_info->seq) )
+		if (!(dollar_info->seq))
 			return (-1);
 		dollar_info->to_free = TRUE;
 		i = ft_strlen((dollar_info->seq));
 		*str += 2;
 	}
 	else
-	{
-		varname_len = 0;
-		while (ft_strchr(delimiters, (*str + 1)[varname_len]) == 0)
-			varname_len++;
-		(dollar_info->seq)  = find_envp_arg(info->envp, *str + 1 , varname_len);
-		if ((dollar_info->seq) )
-			i = ft_strlen((dollar_info->seq));
-		*str += varname_len + 1;
-	}
+		search_var_len_dollar(info, str, dollar_info, &i);
 	*len += i;
 	return (i);
 }
 
-long long no_dollar(char **str, char **seq, unsigned int *len)
+long long	no_dollar(char **str, char **seq, unsigned int *len)
 {
 	long long	i;
 
@@ -60,14 +66,30 @@ long long no_dollar(char **str, char **seq, unsigned int *len)
 	return (i);
 }
 
+int	assign_null_var(t_minishell *info, unsigned int len, char **new_str, t_dollar_info *dollar_info)
+{
+		*new_str = (char *)ft_calloc((len + 1), sizeof(char));
+
+		if (!(*new_str))
+		{
+			info->last_exit_status = errno;
+			if (dollar_info->to_free == TRUE)
+			{
+				free(dollar_info->seq);
+				dollar_info->seq = NULL;
+			}
+			return (MALLOC_ERROR);
+		}
+		(*new_str)[len] = '\0';
+		return (OK);
+
+}
 
 char	*expand_dollar(char *str, unsigned int len, t_minishell *info)
 {
-	char		*new_str;
-	/* char		*seq;
-	bool		to_free; */
+	char			*new_str;
 	t_dollar_info	dollar_info;
-	long long	i;
+	long long		i;
 
 	dollar_info.to_free = FALSE;
 	new_str = NULL;
@@ -79,14 +101,16 @@ char	*expand_dollar(char *str, unsigned int len, t_minishell *info)
 		return (NULL);
 	if (*str == '\0')
 	{
-		new_str = (char *)malloc((len + 1) * sizeof(char));
+		if (assign_null_var(info, len, &new_str, &dollar_info) != OK)
+			return (NULL);
+/* 		new_str = (char *)malloc((len + 1) * sizeof(char));
 		new_str[len] = '\0';
 		if (!new_str)
 		{
 			if (dollar_info.to_free == TRUE)
 				free(dollar_info.seq);
 			return (NULL);
-		}
+		} */
 	}
 	else
 		new_str = expand_dollar(str, len, info);
